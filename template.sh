59e76cb31891ee72a3ec192a8ca154f0a0be9915
moeldeeb1077 <mohamed.el-deeb@capgemini.com>
2026-04-11 01:20:26 +0200

    project setup

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir src
+mkdir research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Setup complete ."
